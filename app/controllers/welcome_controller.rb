938f0d1c1e84b4a5080d138229ab1f020bf4be4a
noumanshah436 <noumanshah436@gmail.com>
2022-09-01 12:39:43 +0500

    helper methods

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,13 @@
 class WelcomeController < ApplicationController
+  # layout 'header'
+
   def index
+    # redirect_to :form
+    # redirect_to action: 'form'
+    @date = Date.today
+  end
+
+  # http://localhost:3000/welcome/form
+  def form
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get 'index', to: 'welcome#index', as: 'index'
+  get 'form', to: 'welcome#form', as: 'form'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
